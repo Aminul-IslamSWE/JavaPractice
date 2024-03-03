398204520bd8de14a40640573549f732bdafc625
Raz2396 <razu2396@gmail.com>
2024-03-04 02:58:26 +0600

    Simple if statement:

---
+++ src/Control/Statements/Student.java
@@ -0,0 +1,12 @@
+package Control.Statements;
+
+// Simple if statement
+public class Student {
+    public static void main(String[] args) {
+        int x=20;
+        int y=23;
+        if (x+y>20){
+            System.out.println("x+y is greater than 20");
+        }
+    }
+}
